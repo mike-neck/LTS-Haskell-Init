16cbf9bf9c1a096778411165f5d70f0533721f06
mike_neck <jkrt3333@gmail.com>
2015-05-19 08:22:28 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# LTS-Haskell-Init
-gradle tasks to setup LTS Haskell
+LTS-Haskell-Init
+===
+
+LTS-Haskell-Init is a gradle tasks for set up LTS Haskell template.
+
+Requirements
+===
+
+* Java 1.8
+* cabal
+
+Usage
+===
+
+* clone this repository or download zip.
+* modify `build.gradle` as you like
+* `./gradlew tasks` shows tasks named `ltsHaskellSetup{version}` which set up LTS Haskell template.
+* The list of versions is retrieved from stackage.org.
+* select LTS Haskell version and run its task.
+
+```
+$ ./gradlew tasks
+------------------------------------------------------------
+All tasks runnable from root project
+------------------------------------------------------------
+
+LTS Haskell (latest: 2.9) tasks
+-------------------------------
+cabalSandboxInit2.2 - Download LTS Haskell 2.2 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.3 - Download LTS Haskell 2.3 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.4 - Download LTS Haskell 2.4 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.5 - Download LTS Haskell 2.5 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.7 - Download LTS Haskell 2.7 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.8 - Download LTS Haskell 2.7 configuration and run init command(cabal sandbox init).
+cabalSandboxInit2.9 - Download LTS Haskell 2.7 configuration and run init command(cabal sandbox init).
+
+$ ./gradlew cabalSandboxInit2.9
+:mkdir2.8
+:download2.8
+:cabalSandboxInit2.8
+Writing a default package environment file to
+/Users/mike/.ltshs/2.8/cabal.sandbox.config
+Creating a new sandbox at /Users/mike/.ltshs/2.8/.cabal-sandbox
+:cabalSetup2.8
+Resolving dependencies...
+Notice: installing into a sandbox located at
+/Users/mike/.ltshs/2.8/.cabal-sandbox
+Configuring ansi-terminal-0.6.2.1...
+...
+Building ghc-mod-5.2.1.2...
+Installed ghc-mod-5.2.1.2
+
+BUILD SUCCESSFUL
+
+Total time: 9 mins 8.758 secs
+```
+
+Configuration
+===
+
+You can configure several settings.
+
+property|meaning
+:--|:--
+`model.ltsHaskell.dir`|The directory where LTS Haskell is set up(default is `$HOME/.ltshs`). The directory must be existing.
+`model.ltsHaskell.cabal.create.install`|A cabal package you need. This parameter can be configured multiple times.
+
+example
+
+```groovy
+model {
+    ltsHaskell {
+        // cabal sandbox init will be executed on /path/to/lts/haskell/version
+        dir = '/path/to/lts/haskell'
+        // happy and ghc-mod will be installed to the directory.
+        cabal.create {
+            install = 'happy'
+        }
+        cabal.create {
+            install = 'ghc-mod'
+        }
+    }
+}
+```
---
+++ buildSrc/src/main/groovy/com/github/mikeneck/LtsHaskell.groovy
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2015 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mikeneck
+
+import org.gradle.api.Task
+import org.gradle.api.tasks.Exec
+import org.gradle.model.Model
+import org.gradle.model.Mutate
+import org.gradle.model.RuleSource
+import org.gradle.model.Validate
+import org.gradle.model.collection.CollectionBuilder
+import org.jsoup.Jsoup
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+import static java.util.stream.Collectors.toSet
+
+class LtsHaskell extends RuleSource {
+
+    static final String STACKAGE_URL = 'https://www.stackage.org/snapshots'
+
+    static final String STACKAGE_SNAPSHOTS_URL = 'https://www.stackage.org/snapshot/lts-'
+
+    static final String CABAL_CONFIG = 'cabal.config'
+
+    static final def CONFIG_URL = {String version ->
+        "http://www.stackage.org/lts/${version}/${CABAL_CONFIG}"
+    }
+
+    static final String LTS_HASKELL = 'LTS Haskell'
+
+    static final def TASK_NAME = {String task, String version ->
+        def v = version
+        "${task}${v}"
+    }
+
+    static final    def GROUP = {String latest ->
+        "${LTS_HASKELL} (latest: ${latest})"
+    }
+
+    static final def DESCRIPTION = {String version ->
+        "Download ${LTS_HASKELL} ${version} configuration and run cabal sandbox init, cabal install."
+    }
+
+    @Model
+    void ltsHaskell(LtsHaskellConfiguration base) {
+        // retrieving directory
+        def home = System.getenv("HOME") == null ?
+                System.getenv("HOMEPATH").replace('\\', '/') :
+                System.getenv("HOME")
+        base.dir = "${home}/.ltshs"
+    }
+
+    @Validate
+    void checkDirectoryExists(LtsHaskellConfiguration base) {
+        def path = Paths.get(base.dir)
+        if(!Files.exists(path)) {
+            throw new IllegalArgumentException($/Directory[${base.dir}] does not exist.
+Please run command before run gradle tasks.
+mkdir -p ${base.dir}
+/$)
+        }
+    }
+
+    @Mutate
+    void createTask(CollectionBuilder<Task> tasks, LtsHaskellConfiguration base) {
+        // modify directory name to end with /
+        def dir = base.dir.endsWith('/') ?
+                base.dir :
+                "${base.dir}/"
+        def path = Paths.get(dir)
+        // filter for haskell lts haskell versions
+        def list = Files.list(path)
+                .filter{Files.isDirectory(it)}
+                .map{it.toAbsolutePath()}
+                .map{it.toString()}
+                .map{it.replace(dir, '')}
+                .collect(toSet())
+        // retrieving recent lts haskell versions
+        def doc = Jsoup.parse(new URL(STACKAGE_URL), 4000)
+        def versions = doc.select('ul.snapshots li a').findAll {
+            it.text().contains(LTS_HASKELL)
+        }.collect {
+            it.attr('href').replace(STACKAGE_SNAPSHOTS_URL, '')
+        }
+        // retrieving latest lts haskell version
+        def latest = versions.sort(false, Comparator.reverseOrder())[0]
+        // filtering haskell versions
+        versions.findAll {
+            !list.contains(it)
+        }.each {String v ->
+            // create directory
+            def mkDir = TASK_NAME('mkdir', v)
+            tasks.create(mkDir) {
+                def newDir = "${dir}/${v}"
+                doLast {
+                    Files.createDirectories(Paths.get(newDir))
+                }
+            }
+            // download config
+            def dl = TASK_NAME('download', v)
+            tasks.create(dl) {
+                def conf = new File("${dir}/${v}/${CABAL_CONFIG}")
+                dependsOn mkDir
+                doLast {
+                    conf.write(new URL(CONFIG_URL(v)).text)
+                }
+            }
+            // cabal sandbox init
+            def sandbox = TASK_NAME('cabalSandboxInit', v)
+            tasks.create(sandbox, Exec) {
+                dependsOn dl
+                workingDir "${dir}/${v}"
+                commandLine 'cabal', 'sandbox', 'init'
+            }
+            // cabal install
+            def cabal = TASK_NAME('ltsHaskellSetup', v)
+            if (base.cabal.size() > 0) {
+                tasks.create(cabal) {
+                    dependsOn sandbox
+                    group = GROUP(latest)
+                    description = DESCRIPTION(v)
+                }
+            } else {
+                tasks.create(cabal, Exec) {
+                    dependsOn sandbox
+                    group = GROUP(latest)
+                    description = DESCRIPTION(v)
+                    workingDir "${dir}/${v}"
+                    def commands = ['cabal', 'install'] + base.cabal.collect {it.install}
+                    commandLine commands
+                }
+            }
+        }
+    }
+}
